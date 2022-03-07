5d719a41f1e262a4db63e49beeae4e06f8ada476
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-07 14:47:39 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+ region = "ap-northeast-2"
+ access_key = var.access_key_var
+ secret_key = var.secret_key_var
+}
+
+resource "aws_instance" "helloworld" {
+  ami = var.ami_var
+  instance_type = var.instance_type_var
+  tags = {
+    Name = var.tags_var
+  }
+}
+
+resource "aws_instance" "helloworld2" {
+  ami = var.ami_var
+  instance_type = var.instance_type_var
+  tags = {
+    Name = var.tags_var
+  }
+}
+
+resource "aws_instance" "helloworld3" {
+  ami = var.ami_var
+  instance_type = var.instance_type_var
+  tags = {
+    Name = var.tags_var
+  }
+}
+
+resource "aws_instance" "helloworld4" {
+  ami = var.ami_var
+  instance_type = var.instance_type_var
+  tags = {
+    Name = var.tags_var
+  }
