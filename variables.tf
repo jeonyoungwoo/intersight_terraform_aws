57c141b87e2090a7f82d7ad54f475e07d9bf1f4b
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-07 14:51:09 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key_var" {
+  type        = string
+}
+variable "secret_key_var" {
+  type        = string
+}
+variable "ami_var" {
+  type        = string
+}
+variable "instance_type_var" {
+  type        = string
+}
+variable "tags_var" {
+  type        = string
+}
