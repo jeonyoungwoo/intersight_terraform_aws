658a9f6834314c22a74c20ae38964e35106b069d
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-12 18:35:51 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,26 @@
 provider "aws" {
+ #region ="us-east-1" //미국-버지니아
+ #region ="" //미국-오하이오
+ #region ="" //미국-캘리포니아
+ #region ="" //미국-오레곤
+ #region ="" //아프리카-케이프타운
+ #region ="" //아시아-홍콩
+ #region ="" //아시아-자가르타
+ #region ="" //아시아-뭄바이
+ #region ="" //아시아-오사카
  region = "ap-northeast-2"            //서울리전.
+ #region ="" //아시아-싱가포르
+ #region ="" //아시아-시드니
+ #region ="" //아시아-도쿄
+ #region ="" //캐나다-중부
+ #region ="" //유럽-프랑크푸르트
+ #region ="" //유럽-아일랜드
+ #region ="" //유럽-런던
+ #region ="" //유럽-밀라노
+ #region ="" //유럽-파리
+ #region ="" //유럽-스톡홀름
+ #region ="" //중동-바레인
+ #region ="" //남아메리카(상파울르)
  access_key = var.access_key_var
  secret_key = var.secret_key_var
 }
